92650e98c987cceb40ee8ed0bb40174d36ffcaa1
Evgeny Alexeenko <ztavruz1987@mail.ru>
2025-05-05 19:37:49 +0300

    set DB user ztavruz

--- client/src/services/Auth_Service.js
+++ client/src/services/Auth_Service.js
@@ -3,18 +3,18 @@ import $api from "../http/index.mjs";
 export default class AuthService {
 
     static async login(email, password) {
-        return $api.post('/users/login', {email, password});
+        return $api.post('/api/users/login', {email, password});
         
     }
 
 
     static async registration(email, password) {
-        return $api.post('/users/registration', {email, password});
+        return $api.post('/api/users/registration', {email, password});
         
     }
 
     static async logout() {
-        return $api.post('/users/logout');
+        return $api.post('/api/users/logout');
         
     }
 
